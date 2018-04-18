bb66d6d8b633fcad561a4a33532df4ff2a5e08a2
NachoRV <royovillanova.i@gmail.com>
2018-04-18 18:10:22 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <button on:clikc='logearme()'></button>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  /* Autentificacion con GitHub */
+var provider: new firebase.auth.GithubAuthProvider();
+function logearme(){
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+
+  });
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
